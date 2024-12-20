7de08aaee5dec3a183c3ead433450e8725212e37
ReizarXPro <115897209+ReizarXPro@users.noreply.github.com>
2024-12-20 23:44:02 +0100

    Update page.js for title

--- src/app/page.js
+++ src/app/page.js
@@ -15,6 +15,7 @@ import {
 export default function Home() {
   return (
     <div className="relative z-0 bg-primary">
+    <title> Rammeh's Portfolio <title/>
       <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
         <Navbar />
         <Hero />
